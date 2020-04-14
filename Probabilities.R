cd866c105f96a5a128fbe601bd0869d1e49a9526
LJBCPH <lucasjohanboesen123@gmail.com>
2020-04-14 21:54:36 +0200

    edefsfsfd

--- Probabilities.R
+++ Probabilities.R
@@ -1,7 +1,6 @@
 rm(list=ls())
 library(matlib)
 #Christoffer er fucking noob
-#Ja han er 
 setwd("C:/Users/lucas/Desktop/Odd")
 #Henter og verificerer data
 data <- read.table("kampe_r.csv",header=T,sep=",")
