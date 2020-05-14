d048c8751556d8875eb63d9b7c89365a556218d7
LJBCPH <lucasjohanboesen123@gmail.com>
2020-05-14 13:32:39 +0200

    prob

--- Probabilities.R
+++ Probabilities.R
@@ -1,14 +1,13 @@
-#git add Probabilities.R #git commit -m "Navn på ændring" #git push #git pull
+#git add Probabilities.R #git commit -m "Navn pÃ¥ Ã¦ndring" #git push #git pull
 rm(list=ls())
 library(matlib) #Til vektor/matrix regning
 library(MASS) # Til invers af numerisk problematiske matricer
-library(blockmatrix) #matrixopsætning
+library(blockmatrix) #matrixopsÃ¦tning
 library(tidyr) #Til data transformation
 library(xtable) #Til Latex table
 library(BTSoccer)
-library(ggplot2)
+library(ggplot2) #plots
 library(psych) #pairs.panel
-library(glmnet)
 library(SSLASSO)
 
 setwd("C:/Users/Victo/Desktop/bachelor/kode")
@@ -20,7 +19,136 @@ data$U <- as.character(data$U)
 data$dato <- as.Date(data$dato, format = "%m/%d/%Y")
 m <- CreateMatrixes(data,"2015-07-17","2016-05-29",30)
 x <- m$DesignMatrix;Y <- m$KontingensTabel; r <- m$SamledeKampe;
-n <- NR(x=x,,Stheta = 1.57,Sbeta = startbeta[-10],lambda=25,MaxIte = 600,eps = 0.0000001,LambLimit = 10^(-4),c = 0.000000000000000001);beta <- n$beta;theta <- n$theta;
+KrydsVal <- cbind(c(4,8),c(9,13),c(14,18),c(19,23),c(24,28),c(29,33))
+LambdaValues <- c(0,0.5,0.75,1,1.25,1.5,2,2.5,3,3.5,4,4.5,5,6,10,12.5,15,17.5,20,25,35)
+for(Lambda in LambdaValues){
+  for(Kryds in 1:6){
+    SS <- c(KrydsVal[1,Kryds],KrydsVal[2,Kryds])
+    RoundList = c(3:(SS[1]-1),(SS[2]+1):33)
+    n <- NR(x=x,,Stheta = 1.57,Sbeta = beta0,lambda=Lambda,RoundList = RoundList,MaxIte = 600,eps = 0.0000001,LambLimit = 10^(-4),c = 0.000000000000000001);beta <- n$beta;theta <- n$theta;
+  }
+}
+TData <- data[which((data$runde >= SS[1] & data$runde <= SS[2])&(data$dato>="2015-07-17") & (data$dato <= "2016-05-29")),]
+#Sandsynligheder ift. beta osv
+a = 0;b=0;aa=0;bb=0;
+aa <- as.matrix(rbind(c(0,0,0)));bb <- as.matrix(rbind(c(0,0,0)));cc <- as.matrix(rbind(c(0,0,0,"H","U")));
+beta <- n$beta;theta <- n$theta
+for (runde in SS[1]:SS[2]){
+  m <- CreateMatrixes(data,"2015-07-17","2016-05-29",runde,TRUE)
+  x <- m$DesignMatrix;Y <- m$KontingensTabel; r <- m$SamledeKampe;f <- BTFunktioner(x=x,Y=Y,r=r);
+  data1 <- data[which((data$dato>="2015-07-17") & (data$dato<="2016-05-29") & (data$runde == runde)),]
+  data1$HU <- paste(data1$H,data1$U)
+  x <- x[which(rownames(x) %in% names(beta)),]
+  styrker <- exp(t(x)%*%beta)
+  #styrker <- statstyrker
+  counter = 1;KumSSH <- matrix(NA, nrow = (factorial(length(UnikHold))/(factorial((length(UnikHold)-2)))), ncol = 5)
+  for (hold1 in 1:length(UnikHold)) {
+    for (hold2 in 1:length(UnikHold)) {
+      if (hold1 != hold2){
+        KumSSH[counter,] <- cbind(Sandsynligheder(theta,x,styrker,hold1,hold2)[1],Sandsynligheder(theta,x,styrker,hold1,hold2)[2],Sandsynligheder(theta,x,styrker,hold1,hold2)[3],UnikHold[hold1],UnikHold[hold2])
+        counter = counter +1
+      }
+    }
+  }
+  KumSSH <- as.data.frame.matrix(KumSSH) #kummuleret ssh
+  names(KumSSH) <- c("H","U","Uafgjort","H1","H2")
+  KumSSH$H1H2 <- paste(KumSSH$H1,KumSSH$H2)
+  ssh <- KumSSH[KumSSH$H1H2 %in% data1$HU,]
+  ssh$H <- as.numeric(as.character(ssh$H));ssh$U <- as.numeric(as.character(ssh$U));ssh$Uafgjort <- as.numeric(as.character(ssh$Uafgjort))
+  ssh <- ssh[order(ssh$H1H2),]
+  data1 <- data1[order(data1$HU),]
+  as.data.frame(lapply((cbind(data1$Hsejr,data1$Usejr,data1$Uafgjort)-cbind(ssh$H,ssh$U,ssh$Uafgjort))^2, function(y) sum(y)))
+  A <- c(rowSums(as.matrix((cbind(data1$Hsejr,data1$Usejr,data1$Uafgjort)-cbind(ssh$H,ssh$U,ssh$Uafgjort))^2))) #(Y-Yhat)^2
+  a <- c(append(a,A,after= length(a)))
+  B <- c(rowSums((cbind(ssh$H,ssh$U,ssh$Uafgjort)^2))) #Yhat^2
+  b <- c(append(b,B,after = length(b)))
+  AA <- (as.matrix((cbind(data1$Hsejr,data1$Usejr,data1$Uafgjort)-cbind(ssh$H,ssh$U,ssh$Uafgjort)))) #Y-Yhat
+  aa <- rbind(aa,AA)
+  BB <- as.matrix(((cbind(ssh$H,ssh$U,ssh$Uafgjort)))) #Yhat
+  bb <- rbind(bb,BB)
+  CC <- as.matrix(((cbind(ssh$H,ssh$U,ssh$Uafgjort,data1$H,data1$U)))) #Yhat med hold
+  cc <- rbind(cc,CC)
+}
+#startbeta = beta
+
+n <- length(beta0)
+#fixer length
+length(beta305) <- n
+#length(Beta0) <- n
+#names(beta305) <- c("FifaRating")
+beta05
+Beta05 <- as.data.frame(beta05,row.names = names(beta05))
+BetaList <- list(Beta0,Beta05,Beta075,Beta1,Beta0125,Beta015,Beta2,Beta25,Beta3,Beta35,Beta4,Beta45,Beta5,Beta6,
+                 Beta10,Beta125,Beta15,Beta175,Beta20,Beta205,Beta305)
+#b1
+#b0125 <- sum(abs(beta));beta0125 <- beta #3  -183.8938, 9 beta
+#b015 <- sum(abs(beta));beta015 <- beta #3 -184.1924, 8 beta
+#b2 <- sum(abs(beta));beta2 <- beta #4, -184.7497, 8 beta
+#b25 <- sum(abs(beta));beta25 <- beta, #-185.2661, 7 beta
+#b3 <- sum(abs(beta));beta3 <- beta, #-185.7535, 7 beta
+#b35 <- sum(abs(beta));beta35 <- beta #10, -186.2123, 7 beta
+#b4 <- sum(abs(beta));beta4 <- beta #15, -186.6428, 7 beta
+#b45 <- sum(abs(beta));beta45 <- beta #15, -186.0451, 6 beta
+#b5 <- sum(abs(beta));beta5 <- beta #20, -187.43, 5 beta
+#b6 <- sum(abs(beta));beta6 <- beta #25, -188.1695, 5 beta
+#b10 <- sum(abs(beta));beta10 <- beta #30, -190.7334, 5 beta
+#b125 <- sum(abs(beta));beta125 <- beta #85,-192.1757, 4 beta
+#b15 <- sum(abs(beta));beta15 <- beta #85,-190.7295, 3 beta
+#b175 <- sum(abs(beta));beta175 <- beta #85,-192.2547, 3 beta
+#b20 <- sum(abs(beta));beta20 <- beta #85,-194.8933, 3 beta
+#b205 <- sum(abs(beta));beta205 <- beta #16,-193.4999, 2 beta
+#beta305 <- c(0.03629097) #-198.5425, 1 beta
+
+#library(plyr)
+#join_all(BetaList,type="full")
+
+for(i in 1:length(BetaList)){
+  colnames(BetaList[[i]]) <- paste0( names(BetaList)[i], "_", colnames(BetaList[[i]]) )
+  BetaList[[i]]$ROWNAMES  <- rownames(BetaList[[i]])
+}
+length(BetaList)
+m1 <- merge(BetaList[1],BetaList[2],by="ROWNAMES",all=T)
+
+merge(m1,BetaList[3],by="ROWNAMES",all=T)
+for(i in 3:21){
+  m1 <- merge(m1,BetaList[i],by="ROWNAMES",all=T)
+  #m1 <- m1[(length(m1[,1])-11):length(m1[,1]),]
+  m1 <- m1[which(m1$ROWNAMES!=""),]
+}
+m2 <- m1[-18]
+m2 <- m2[-10]
+m2[is.na(m2)] = 0
+m3 <- as.data.frame(t(m2))
+colnames(m3) = m2$ROWNAMES
+m3 <- m3[-1,]
+m3 <- as.data.frame(m3)
+LambdaValues <- c(0,0.5,0.75,1,1.25,1.5,2,2.5,3,3.5,4,4.5,5,6,10,12.5,15,17.5,20,25,35)
+LambdaValues <- LambdaValues[-18]
+LambdaValues <- LambdaValues[-10]
+#rownames(m3) <- c(LambdaValues)
+m3$Lambda <- c(LambdaValues)
+m4 <- read.table(file.choose(),header=T,sep=",")
+names(m4) <- c("X","Lambda","variable","value")
+names(m4)
+m4 <- melt(m3,id.vars="Î»")
+m4$value <- as.numeric(m4$value)
+m4
+
+ggplot(m4, aes(Lambda,value, col=variable)) +
+  geom_line() + ylab("Koefficienter") +xlab("Î»") +  #expand_limits(x = c(LambdaValues))
+  geom_hline(aes(yintercept= 0), colour= 'white',size=0.1) +
+  scale_x_continuous(breaks = c(0,5,10,15,20,25,30,35,47))+
+  scale_color_manual(values = c("coral4","dodgerblue4","antiquewhite4","deeppink1","chartreuse1","slateblue",
+                                "burlywood","magenta1","blue1","orangered1","turquoise3","yellowgreen")) +
+#scale_fill_viridis_d() +
+  geom_dl(aes(label = variable), method = list(dl.combine("first.points"), cex = 0.8))
+
+  scale_fill_brewer(palette="Spectral",guide="none")
+scale_linetype_manual(name = "Observerede %-Udfald", values = c(rep(2,12)),
+                      guide = guide_legend(override.aes = list(color = c("red", "blue","green"))))
+
+#write.csv(m4, file = "beta_Lasso.csv")
+
 #b0 <- sum(abs(startbeta));beta0 <- startbeta #0, -181.968, 12 beta
 #b05 <- sum(abs(beta));beta05 <- beta #1 -182.8425, 11 beta
 #b075 <- sum(abs(beta));beta075 <- beta #2 -183.0041, 10 beta
@@ -41,93 +169,7 @@ n <- NR(x=x,,Stheta = 1.57,Sbeta = startbeta[-10],lambda=25,MaxIte = 600,eps = 0
 #b175 <- sum(abs(beta));beta175 <- beta #85,-192.2547, 3 beta
 #b20 <- sum(abs(beta));beta20 <- beta #85,-194.8933, 3 beta
 #b205 <- sum(abs(beta));beta205 <- beta #16,-193.4999, 2 beta
-##b17 <- sum(abs(beta));beta17 <- beta #125, 0.0000000000001,0,-193.9977, 3 beta
-#b18 <- sum(abs(beta));beta18 <- beta #150, 0.0000000000001, 0, -194.3907, 3 beta
-##b19 <- sum(abs(beta));beta19 <- beta #50, 0.00000001, 0, -194.6659, 3 beta
-#b20 <- sum(abs(beta));beta20 <- beta #50, 0.000000001, 0, -194.9399, 3 beta
-#b22 <- sum(abs(beta));beta22 <- beta #50, 0.0000000000001, 0 ,-195.4818 , 3 beta
-#b25 <- sum(abs(beta));beta25 <- beta #50, 0.0000000000001, 0 ,-195.4818 , 2 beta
-#b25 <- sum(abs(beta));beta25 <- beta #50, 0.0000000000001, 0 ,-197.0296 , 2 beta
-#beta35 <- c(0.01903043)
-
-colSums(Bsamlet)
-#startbeta = beta
-LambdaValues <- c(0,0.5,1,1.5,2,3,3.5,4,5,6,7,10,13,13.5,16,18,20,22,25)
-n <- length(beta0)
-#fixer length
-#length(beta075) <- n
-#length(Beta0) <- n
-#names(beta4) <- c("FifaRating")
-beta1
-Beta075 <- as.data.frame(beta075,row.names = names(beta075))
-BetaList <- list(Beta0,Beta075,Beta1,Beta15,Beta2,Beta3,Beta4,Beta5,Beta6,Beta7,Beta10,Beta13,Beta135,Beta16,Beta18,Beta20,Beta22,Beta25,Beta35)
-#multimerge(BetaList)
-#unique(unlist(lapply(BetaList, rownames)))
-#library(plyr)
-#join_all(BetaList,type="full")
-for(i in 1:length(BetaList)){
-  colnames(BetaList[[i]]) <- paste0( names(BetaList)[i], "_", colnames(BetaList[[i]]) )
-  BetaList[[i]]$ROWNAMES  <- rownames(BetaList[[i]])
-}
-
-m1 <- merge(BetaList[1],BetaList[2],by="ROWNAMES",all=T)
-for(i in 3:20){
-m1 <- merge(m1,BetaList[i],by="ROWNAMES",all=T)
-m1 <- m1[(length(m1[,1])-11):length(m1[,1]),]
-}
-m2 <- m1[-16]
-m2 <- m2[-4]
-m2[is.na(m2)] = 0
-m3 <- as.data.frame(t(m2))
-colnames(m3) = m2$ROWNAMES
-m3 <- m3[-1,]
-m3 <- as.data.frame(m3)
-m3$Lambda <- as.numeric(row.names(m3))
-m4 <- melt(m3,id.vars="Lambda")
-m4$value <- as.numeric(m4$value)
-ggplot(m4, aes(Lambda,value, col=variable)) +
-  geom_line() + geom_hline()
-
-#b0 <- sum(abs(beta));beta0 <- beta #0, 0.0000000000001, -181.968, 12 beta
-#b075 <- sum(abs(beta));beta075 <- beta #1 0.000000000001, 0, -182.1556, 11 beta
-#b1 <- sum(abs(beta));beta1 <- beta #2 0.000000000001, 0, -183.5661, 10 beta
-#b15 <- sum(abs(beta));beta15 <- beta #3 0.000000000001, 0, -184.189, 10 beta
-#b2 <- sum(abs(beta));beta2 <- beta #3 0.000000000001, 0, -184.7619, 9 beta
-##b25 <- sum(abs(beta));beta25 <- beta #4, 0.000000000001, 0, -185.2501, 8 beta
-#b3 <- sum(abs(beta));beta3 <- beta #5, 0.000000000001, 0, -185.7342, 8 beta
-##b35 <- sum(abs(beta));beta35 <- beta #10, 0.000000000001, 0, -186.1894, 8 beta
-#b4 <- sum(abs(beta));beta4 <- beta #15, 0.000000000001, 0, -186.6159, 7 beta
-#b5 <- sum(abs(beta));beta5 <- beta #20,0.0000000000001, 0, -187.4058, 6 beta
-#b6 <- sum(abs(beta));beta6 <- beta #25,0.0000000000001, 0, -188.1516, 6 beta
-#b7 <- sum(abs(beta));beta7 <- beta #30,0.0000000000001, 0, , 6 beta
-#b10 <- sum(abs(beta));beta10 <- beta #85, 0.000000000001,0,-190.7295, 6 beta
-#b13 <- sum(abs(beta));beta13 <- beta #85, 0.000000000001,0,-192.2547, 5 beta
-#b135 <- sum(abs(beta));beta135 <- beta #85, 0.000000000001,0,-192.4757, 4 beta
-#b16 <- sum(abs(beta));beta16 <- beta #16, 0.0000000000001, 0,-193.4999, 4 beta
-##b17 <- sum(abs(beta));beta17 <- beta #125, 0.0000000000001,0,-193.9977, 3 beta
-#b18 <- sum(abs(beta));beta18 <- beta #150, 0.0000000000001, 0, -194.3907, 3 beta
-##b19 <- sum(abs(beta));beta19 <- beta #50, 0.00000001, 0, -194.6659, 3 beta
-#b20 <- sum(abs(beta));beta20 <- beta #50, 0.000000001, 0, -194.9399, 3 beta
-#b22 <- sum(abs(beta));beta22 <- beta #50, 0.0000000000001, 0 ,-195.4818 , 3 beta
-#b25 <- sum(abs(beta));beta25 <- beta #50, 0.0000000000001, 0 ,-195.4818 , 2 beta
-#b25 <- sum(abs(beta));beta25 <- beta #50, 0.0000000000001, 0 ,-197.0296 , 2 beta
-#beta35 <- c(0.01903043)
-
-round(beta1,4)#0
-round(beta2,4)#0.00000001
-round(beta4,4)#0.03
-round(beta5,4)#0.04
-round(beta3,4)#0.05
-sum(abs(beta1))#25,c=0,1
-sum(abs(beta2))#100,c=0,01
-sum(abs(beta3))#0.3
-sum(abs(beta4))#2
-sum(abs(beta5))#5
-sum(abs(beta6))#10
-sum(abs(beta7))#50
-sum(abs(beta8))#100
-sum(abs(beta9))#200
-sum(abs(beta10))#200
+#b35 <- c(0.03629097) #-198.5425, 1 beta
 
 alpha=33
 styrker = 0;y = 0;R=0;X=0;FF=0;SamlStyrker=0;
@@ -166,7 +208,7 @@ data1 <- data[which((data$dato>="2015-07-17") & (data$dato<="2016-05-29")),]
 UnikHold <- unique(data1$H)
 UnikHold <- sort(UnikHold)
 runde = 3
-#Danner sandsynligheder tilhørende kampene
+#Danner sandsynligheder tilhÃ¸rende kampene
 m <- CreateMatrixes(data,"2015-07-17","2016-05-29",4,TRUE) #laver design, kontingens og r-tabel
 a = 0;b=0;aa=0;bb=0;
 aa <- as.matrix(rbind(c(0,0,0)));bb <- as.matrix(rbind(c(0,0,0)));cc <- as.matrix(rbind(c(0,0,0,"H","U")));
@@ -303,7 +345,7 @@ Lh0 = -210.9386
 chi1 = -2*(Lh0-Lmle)
 chi2 = pchisq(chi1,12,lower = F);round(chi2,5)
 
-#Standardfejl på styrker
+#Standardfejl pÃ¥ styrker
 dlpipi <- function(styrker,theta,Y,r,i){
   sum=0;
   for(j in 1:length(styrker)) {
